26fae15748da60a513c6c6a436b95b7a5d62ac72
alchemistmatt <matthew.monroe@pnnl.gov>
2018-10-23 18:05:25 -0700

    Update the batch file

--- MyEMSLDownloader/bin/Distribute_Files.bat
+++ MyEMSLDownloader/bin/Distribute_Files.bat
@@ -1,8 +1,10 @@
 @echo off
 
+pushd F:\Documents\Projects\DataMining\DMS_Managers\MyEMSL_Reader\MyEMSLDownloader\bin
 call Distribute_Files_Work.bat C:\DMS_Programs\MyEMSLDownloader
 call Distribute_Files_Work.bat \\floyd\software\MyEMSLDownloader
 call Distribute_Files_Work.bat \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution\MyEMSLDownloader
 call Distribute_Files_Work.bat \\pnl\projects\OmicsSW\DMS_Programs\CaptureTaskManagerDistribution\MyEMSLDownloader
+popd
 
 pause
