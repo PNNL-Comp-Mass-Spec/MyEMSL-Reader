8ec187d41eb8ed9b83a2fc110cc2c24ce70f417c
alchemistmatt <matthew.monroe@pnnl.gov>
2018-10-25 19:39:07 -0700

    Updated DLL

--- MyEMSLDownloader/Program.cs
+++ MyEMSLDownloader/Program.cs
@@ -15,7 +15,7 @@ namespace MyEMSLDownloader
 
     internal static class Program
     {
-        private const string PROGRAM_DATE = "October 23, 2018";
+        private const string PROGRAM_DATE = "October 24, 2018";
 
         static double mPercentComplete;
         static DateTime mLastProgressUpdateTime = DateTime.UtcNow;
