198eb416a7ad3b9c6f55c29af81891d06aaffd40
alchemistmatt <matthew.monroe@pnnl.gov>
2021-01-04 13:47:31 -0800

    Updated distribution files

--- MyEMSLDownloader/bin/Distribute_Files.bat
+++ MyEMSLDownloader/bin/Distribute_Files.bat
@@ -6,10 +6,10 @@ call Distribute_Files_Work.bat \\floyd\software\MyEMSLDownloader
 call Distribute_Files_Work.bat \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution\MyEMSLDownloader
 call Distribute_Files_Work.bat \\pnl\projects\OmicsSW\DMS_Programs\CaptureTaskManagerDistribution\MyEMSLDownloader
 
-xcopy debug\MyEMSLReader.dll  "F:\Documents\Projects\DataMining\DMS_Programs\DMS_Dataset_Retriever\Lib" /D /Y
-xcopy debug\Pacifica.Core.dll "F:\Documents\Projects\DataMining\DMS_Programs\DMS_Dataset_Retriever\Lib" /D /Y
+xcopy Debug\MyEMSLReader.dll  "F:\Documents\Projects\DataMining\DMS_Programs\DMS_Dataset_Retriever\Lib" /D /Y
+xcopy Debug\Pacifica.Core.dll "F:\Documents\Projects\DataMining\DMS_Programs\DMS_Dataset_Retriever\Lib" /D /Y
 
-rem xcopy debug\MyEMSLReader.dll  "F:\Documents\Projects\DataMining\DMS_Programs\DMS_Dataset_Retriever\Lib" /D /Y
+rem xcopy Debug\MyEMSLReader.dll  "F:\Documents\Projects\DataMining\DMS_Programs\DMS_Dataset_Retriever\Lib" /D /Y
 
 popd
 
