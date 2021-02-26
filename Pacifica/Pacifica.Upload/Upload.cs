5ee292094752813691b4476fdbbced02401c05d6
alchemistmatt <matthew.monroe@pnnl.gov>
2021-02-26 14:38:33 -0800

    Check for and skip duplicate files

--- Pacifica/Pacifica.Upload/Upload.cs
+++ Pacifica/Pacifica.Upload/Upload.cs
@@ -291,11 +291,16 @@ namespace Pacifica.Upload
             foreach (var file in fileList)
             {
                 var fio = new FileInfoObject(file.AbsoluteLocalPath, file.RelativeDestinationDirectory, file.Sha1HashHex);
+
                 if (fileListObject.ContainsKey(file.AbsoluteLocalPath))
                 {
-                    OnWarningEvent("The fileListObject dictionary already has an entry for " + file.AbsoluteLocalPath);
-                    throw new System.Data.Linq.DuplicateKeyException(file.AbsoluteLocalPath);
+                    // Duplicate file
+                    //   This can happen if a dataset has two CacheInfo.txt files that point to the same file
+                    //   For example, Dataset.mzML.gz_CacheInfo.txt referring to \\proto-11\MSXML_Cache\Mz_Refinery_1_230\2020_4\Dataset.mzML.gz
+                    // Skip the duplicate
+                    continue;
                 }
+
                 fileListObject.Add(file.AbsoluteLocalPath, fio);
             }
 
