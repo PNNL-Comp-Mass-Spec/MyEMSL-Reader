0c1c48136fde702623be6fdbbc610e6f719ab4ae
alchemistmatt <matthew.monroe@pnnl.gov>
2021-02-26 16:15:01 -0800

    Update suppressed warning

--- MyEMSLReader/GlobalSuppressions.cs
+++ MyEMSLReader/GlobalSuppressions.cs
@@ -6,4 +6,4 @@
 using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("Usage", "RCS1246:Use element access.", Justification = "Prefer to use .First()", Scope = "module")]
-[assembly: SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "Keep for reference", Scope = "member", Target = "~M:MyEMSLReader.Downloader.DownloadAndExtractTarFile(System.Net.CookieContainer,System.Collections.Generic.List{MyEMSLReader.ArchivedFileInfo},System.Int64,System.Collections.Generic.IReadOnlyDictionary{System.Int64,System.String},System.IO.FileSystemInfo,MyEMSLReader.Downloader.DownloadLayout,System.String,System.Int32)~System.Boolean")]
+[assembly: SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "Keep for reference", Scope = "member", Target = "~M:MyEMSLReader.Downloader.DownloadAndExtractTarFile(System.Net.CookieContainer,System.Collections.Generic.IReadOnlyCollection{MyEMSLReader.ArchivedFileInfo},System.Int64,System.Collections.Generic.IReadOnlyDictionary{System.Int64,System.String},System.IO.FileSystemInfo,MyEMSLReader.Downloader.DownloadLayout,System.String,System.Int32)~System.Boolean")]
