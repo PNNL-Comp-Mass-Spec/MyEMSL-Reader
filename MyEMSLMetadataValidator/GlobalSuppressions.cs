3509206bd9fb878b01f033b6d2ee814110a7c4f6
alchemistmatt <matthew.monroe@pnnl.gov>
2023-04-18 15:34:52 -0700

    Update suppressed warnings

---
+++ MyEMSLMetadataValidator/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Roslynator", "RCS1123:Add parentheses when necessary.", Justification = "Parentheses not required", Scope = "member", Target = "~M:MyEMSLMetadataValidator.MetadataValidator.ValidateDatasetBatch(PRISMDatabaseUtils.IDBTools,System.IO.TextWriter,System.Collections.Generic.ICollection{System.Int32},System.Int32,System.Int32,System.Int32,System.Single)~System.Boolean")]
